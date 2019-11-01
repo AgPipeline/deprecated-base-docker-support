9b190cb36e6ac6573bf81adeb84278dcfe02968d
Sithyphus <jorgebarrios@email.arizona.edu>
2019-11-01 12:00:39 -0700

    Changed the tests from base unittest to pytest

--- base-image/test_transformer.py
+++ base-image/test_transformer.py
@@ -1,56 +1,35 @@
 """Tests for transformer.py
 """
 
-#Import testing module
-import unittest
-
-#Import transformer_class.py module and inbedded modules
+#Import transformer.py module and imbedded modules
 import transformer
-import transformer_class
+from transformer_class import Transformer
 import argparse
 
+#Initial testing values
+test_transformer = Transformer()
+parse = argparse.ArgumentParser()
 
-#Create testing class
-class TransformerTest(unittest.TestCase):
-    """Generic Class for running tests
+def test_add_parameters():
+    """Testing add_parameters function
     """
-    def setUp(self):
-        """Sets up an initial instance of the transformer class
-        """
-        self.transformer = transformer_class.Transformer()
+    #Saving function call to variable
+    testee = transformer.add_parameters(test_transformer)
+    #Should return None
+    assert testee == None
 
-    def test_add_parameters(self):
-        """
-        Testing add_parameters function
-        """
-        #create an argparse instance
-        parse = argparse.ArgumentParser(description= "Test")
-        
-        #Checks that add_parameters returns None
-        testee =transformer.add_parameters(parse) 
-        self.assertIsNone(testee, 'Does not return None')
-        
+def test_check_continue():
+    """Testing check_continue function
+    """
+    #Saving function call to variable
+    testee = transformer.check_continue(test_transformer)
+    #Should return dict type
+    assert isinstance(testee, dict)
 
-    def test_check_continue(self):
-        """Testing check_continue function
-        """
-        
-        #Storing function call to a variable
-        testee = transformer.check_continue(self.transformer)
-        self.assertIsInstance(testee, dict, "Should return dict type")
-
-    def test_perform_process(self):
-        """Test of the perform_process function
-        """
-        
-        #storing function call to variable
-        testee = transformer.perform_process(self.transformer)
-        self.assertIsInstance(testee, dict, "Should return dictionary type")
-        
-        
-        
-
-
-
-
-    
+def test_perform_process():
+    """Test of the perform_process function
+    """
+    #Storing function call to a variable
+    testee = transformer.perform_process(test_transformer)
+    #Should return dict type
+    assert isinstance(testee, dict)
--- base-image/test_transformer_class.py
+++ base-image/test_transformer_class.py
@@ -1,60 +1,27 @@
 """Tests for 'transformer_class.py"
 """
 
-#Import testing module
-import unittest
-
-#Import transformer_class.py module and inbedded modules
+#Import transformer_class.py module and inmedded modules
 from transformer_class import Transformer
 import argparse
 
+#Initial testing values
+test_transformer = Transformer()
+parse = argparse.ArgumentParser(description = 'Test')
+test_metadata = {}
 
-#Create testing class
-class TransformerClassTest(unittest.TestCase):
-    """Generic Class for running tests
+def test_add_parameters():
+    """Test for add_parameters function
     """
-    def setUp(self):
-        """Sets up an initial instance of the class
-        """
-        self.transformer = Transformer()
-
-    def test_add_parameters(self):
-        """
-        Testing add_parameters method
-        """
-        #create an argparse instance
-        parse = argparse.ArgumentParser(description= "Test")
-        
-        #Checks that add_parameters returns None
-        self.assertIsNone(self.transformer.add_parameters(parse),\
-            'Does not return None')
-        
-        #Similar only stored in a variable
-        testee = self.transformer.add_parameters(parse)
-        self.assertIsNone(testee, "")
-
-
-
-    def test_get_transformer_params(self):
-        """Testing transformer_params method
-        """
-        #create an argparse.Namespace instance
-        parse = argparse.Namespace
-        #create test dictionary
-        test_data = {}
-        
-        #Tests to make sure return value is dict type
-        self.assertIsInstance(self.transformer.get_transformer_params(parse,test_data), dict, \
-            "Did not return a dictionary object type")
-        
-        #Similar only stored in a vaiable
-        testee = self.transformer.get_transformer_params(parse,test_data)
-        self.assertIsInstance(testee, dict, "Not a dictionary")
-        
-        
-        
-
-
-
-
+    #Saving method call to variable
+    testee = test_transformer.add_parameters(parse)
+    #Should return None
+    assert testee == None
     
+def test_transformer_params():
+    """Test for transformer params
+    """
+    #Saving method call to variable
+    testee = test_transformer.get_transformer_params(parse,test_metadata)
+    #Should return dict type
+    assert isinstance(testee, dict)
