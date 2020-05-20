8216042eba60a2a834b0846f645975c1b43e3ca4
Jacob van der Leeuw <jvanderleeuw@email.arizona.edu>
2020-05-20 16:42:22 -0700

    ran pylint and tested imports

--- base-image/test-files/test_transformer.py
+++ base-image/test-files/test_transformer.py
@@ -1,37 +1,39 @@
 """Tests for transformer.py
 """
 
-#Import transformer.py module and imbedded modules
+# Import transformer.py module and imbedded modules
 import argparse
 import transformer
 from transformer_class import Transformer
 
-
-#Initial testing values
+# Initial testing values
 TEST_TRANSFORMER = Transformer()
 PARSE = argparse.ArgumentParser()
+
 
 # pylint: disable=assignment-from-no-return
 def test_add_parameters():
     """Testing add_parameters function
     """
-    #Saving function call to variable
+    # Saving function call to variable
     testee = transformer.add_parameters(TEST_TRANSFORMER)
-    #Should return None
+    # Should return None
     assert testee is None
+
 
 def test_check_continue():
     """Testing check_continue function
     """
-    #Saving function call to variable
+    # Saving function call to variable
     testee = transformer.check_continue(TEST_TRANSFORMER)
-    #Should return dict type
+    # Should return dict type
     assert isinstance(testee, dict)
+
 
 def test_perform_process():
     """Test of the perform_process function
     """
-    #Storing function call to a variable
+    # Storing function call to a variable
     testee = transformer.perform_process(TEST_TRANSFORMER)
-    #Should return dict type
+    # Should return dict type
     assert isinstance(testee, dict)
